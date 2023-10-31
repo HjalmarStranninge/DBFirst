05df2802836b5cc9ad6924b29ce2ae88ba44b4e3
Hjalmar Stranninge <Hjalmar.stranninge@outlook.com>
2023-10-31 15:06:29 +0100

    Add project files.

---
+++ DBFirst/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace DBFirst
+{
+
+    // Data Source=(localdb)\.;Initial Catalog=NorthWind;Integrated Security=True
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
